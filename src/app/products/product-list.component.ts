6f60508091868c603e1f6ff5a8615920f5c2b3ea
SteveMustanski <steve.mustanski@gmail.com>
2021-02-04 15:44:00 -0500

    added filter logic

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -13,7 +13,16 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value:string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  filteredProducts: IProduct[];
   products: IProduct[] = [
     {
       "productId": 1,
@@ -56,11 +65,21 @@ export class ProductListComponent implements OnInit {
     },
   ];
 
+  constructor() {
+    this.filteredProducts = this.products;
+  }
+
   ngOnInit(): void {
     console.log('In OnInit');
   }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy: string) :IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
   }
 }
